2c266b924099a6b19b5b2855e950984d7dc9b044
Angelica Mapeso <46282803+angelicamapeso@users.noreply.github.com>
2020-11-01 15:29:21 -0500

    Add main menu function
    Displays main menu and returns main menu choices

--- index.js
+++ index.js
@@ -24,7 +24,55 @@ async function connect() {
 }
 
 async function runApp() {
-  //getMainMenuChoice()
+  const mainMenuChoice = await getMainMenuChoice();
+  console.log(mainMenuChoice);
   //switch case options
   //return action
+}
+
+async function getMainMenuChoice() {
+  const mainMenuChoice = await inquirer.prompt([
+    {
+      type: 'list',
+      name: 'table',
+      message: 'Which would you like to modify?',
+      choices: [
+        'Departments',
+        'Roles',
+        'Employees',
+      ],
+    },
+    {
+      type: 'list',
+      name: 'action',
+      message: 'What would you like to do with Departments?',
+      choices: [
+        'Add department',
+        'View departments',
+      ],
+      when: (currentAnswers) => { return currentAnswers.table === 'Departments'},
+    },
+    {
+      type: 'list',
+      name: 'action',
+      message: 'What would you like to do with Roles?',
+      choices: [
+        'Add role',
+        'View roles',
+      ],
+      when: (currentAnswers) => { return currentAnswers.table === 'Roles'},
+    },
+    {
+      type: 'list',
+      name: 'action',
+      message: 'What would you like to do with Employees?',
+      choices: [
+        'Add employee',
+        'View employees',
+        'Update employee role'
+      ],
+      when: (currentAnswers) => { return currentAnswers.table === 'Employees'},
+    },
+  ]);
+  return mainMenuChoice;
 }
