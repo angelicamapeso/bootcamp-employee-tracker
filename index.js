222c225963d9b3d8ec6f39b010770a076c726be1
Angelica Mapeso <46282803+angelicamapeso@users.noreply.github.com>
2020-11-03 11:56:23 -0500

    Remove inquirer require
    UI placed in different file

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const mysql = require('mysql2/promise');
-const inquirer = require('inquirer');
 const consoleTable = require('console.table');
 
 const connectionDetails = require('./assets/connectionDetails.js');
