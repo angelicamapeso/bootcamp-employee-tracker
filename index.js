c55ff3c11226349c904008295192be63d20984c0
Angelica Mapeso <46282803+angelicamapeso@users.noreply.github.com>
2020-11-02 23:57:14 -0500

    Replace main menu func with prompt main menu

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ async function connect() {
 }
 
 async function runApp() {
-  const action = await getMainMenuChoice();
+  const action = await prompts.mainMenu();
   switch(action) {
     case 'Add department':
       console.log('Adding department ...');
@@ -68,53 +68,6 @@ async function runApp() {
     default:
       throw new Error('Invalid main menu action.');
   }
-}
-
-async function getMainMenuChoice() {
-  const mainMenuChoices = await inquirer.prompt([
-    {
-      type: 'list',
-      name: 'table',
-      message: 'Which would you like to modify?',
-      choices: [
-        'Departments',
-        'Roles',
-        'Employees',
-      ],
-    },
-    {
-      type: 'list',
-      name: 'action',
-      message: 'What would you like to do with Departments?',
-      choices: [
-        'Add department',
-        'View departments',
-      ],
-      when: currentAnswers => currentAnswers.table === 'Departments',
-    },
-    {
-      type: 'list',
-      name: 'action',
-      message: 'What would you like to do with Roles?',
-      choices: [
-        'Add role',
-        'View roles',
-      ],
-      when: currentAnswers => currentAnswers.table === 'Roles',
-    },
-    {
-      type: 'list',
-      name: 'action',
-      message: 'What would you like to do with Employees?',
-      choices: [
-        'Add employee',
-        'View employees',
-        'Update employee role'
-      ],
-      when: currentAnswers => currentAnswers.table === 'Employees',
-    },
-  ]);
-  return mainMenuChoices.action;
 }
 
 async function askDepartmentInfo() {
