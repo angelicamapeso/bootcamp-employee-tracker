8cce8d227ca9cc6ad72b9cd6f6beb1ba64128a29
Angelica Mapeso <46282803+angelicamapeso@users.noreply.github.com>
2020-11-04 12:04:33 -0500

    Add descriptor comments in main menu choices

--- assets/constants/mainMenuChoices.js
+++ assets/constants/mainMenuChoices.js
@@ -1,3 +1,5 @@
+//Constants to be used in the main menu and
+//accessed as function options in index
 const mainMenuChoices = {
   tables: ['Departments', 'Roles', 'Employees'],
   departmentActions: ['Add department', 'View departments'],
