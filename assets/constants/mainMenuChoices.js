455975c13e8588148de25608302e5c6b55d2dbb7
Angelica Mapeso <46282803+angelicamapeso@users.noreply.github.com>
2020-11-04 11:55:35 -0500

    Add main menu choice constants
    Shared with index

---
+++ assets/constants/mainMenuChoices.js
@@ -0,0 +1,8 @@
+const mainMenuChoices = {
+  tables: ['Departments', 'Roles', 'Employees'],
+  departmentActions: ['Add department', 'View departments'],
+  roleActions: ['Add role', 'View roles'],
+  employeeActions: ['Add employee', 'View employees', 'Update employee role'],
+}
+
+module.exports = mainMenuChoices;
