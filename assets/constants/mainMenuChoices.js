bfcc01a02cd09c7fe56b0eddcec9a60ec73e7fcb
Angelica Mapeso <46282803+angelicamapeso@users.noreply.github.com>
2020-11-05 12:15:56 -0500

    Add delete employee main menu action

--- assets/constants/mainMenuChoices.js
+++ assets/constants/mainMenuChoices.js
@@ -4,7 +4,7 @@ const mainMenuChoices = {
   tables: ['Departments', 'Roles', 'Employees'],
   departmentActions: ['Add department', 'View departments', 'Delete department'],
   roleActions: ['Add role', 'View roles', 'Delete role'],
-  employeeActions: ['Add employee', 'View employees', 'Update employee role'],
+  employeeActions: ['Add employee', 'View employees', 'Delete employee', 'Update employee role'],
   quit: 'Quit',
 }
 
